46bb247f91791be890a657be1ceff4f0bc4c4155
Abdulla <94592483+Prouzad@users.noreply.github.com>
2022-01-20 14:26:30 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let ar =  Math.abs(n).toString().split(""); //['4', '5', '6']   
+    
+    let rev = ar.reverse().join(""); 
+    console.log(rev);
+    return +rev;
 }
